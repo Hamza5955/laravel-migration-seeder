be15adefc1c14b44cba7bd3aedb09d04223b89bb
Hamza <Hamza5955>
2021-11-02 00:25:02 +0100

    creazione table

---
+++ database/migrations/2021_11_01_231217_create_viggo_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViggoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('viggo', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure', 150);
+            $table->string('destination', 150);
+            $table->string('cuntray', 50);
+            $table->date('daparture_date');
+            $table->string('mail', 50); 
+            $table->integer('phone');
+            $table->date('return_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('viggo');
+    }
+}
